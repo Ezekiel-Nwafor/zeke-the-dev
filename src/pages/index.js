eaf49f4f4b16b5ef9c334c99698e3304ee2315a7
Ezekiel-Nwafor <29420639+Ezekiel-Nwafor@users.noreply.github.com>
2019-10-06 20:34:58 -0400

    installed and configed typography.js installed styled components basic added additional pages (about, blog, and contact) made necessary changes to gatsby config file building header component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+const index = () => {
+    return(
+        <div>
+            <h1>Welcome to my blog!</h1>
+            <p>I am Ezekiel Nwafor (call me Zeke). I'm a developer living in Massachusetts. I like to share the things that I'm learning and doing in web development. </p>
+        </div>
+    )
+}
+
+export default index;
