4a06e3eafbd64739ca828cc20dc2b25506e2936c
Ezekiel-Nwafor <29420639+Ezekiel-Nwafor@users.noreply.github.com>
2019-10-22 08:39:35 -0400

    completed styling header and footer created and added layout component

--- src/components/header.js
+++ src/components/header.js
@@ -2,9 +2,35 @@ import React from 'react';
 import { Link } from 'gatsby'
 import styled from 'styled-components'
 
-const StyledLink = styled(Link)`
+const MenuRight = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+
+const MenuRightFirstLink = styled(Link)`
     text-decoration-line: none;
 `
+const MenuRightMidLink = styled(Link)`
+    text-decoration-line: none;
+    margin-left: 15px;
+    margin-right: 15px;
+`
+
+const MenuRightEndLink = styled(Link)`
+    text-decoration-line: none;
+    margin-right: 40px;
+`
+
+const MenuLeft = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-left: 40px;
+`
+
+const MenuLeftLink = styled(Link)`
+    text-decoration-line: none;
+`
+
 const StyledHeader = styled.header`
     height: 10%;
     width: 100%;
@@ -12,15 +38,25 @@ const StyledHeader = styled.header`
     position: fixed;
 `
 
+const StyledNav = styled.nav`
+    display: flex;
+    justify-content: space-between;
+`
+
 const Header = () => {
     return(
         <StyledHeader>
-            <nav>
-                <StyledLink to='/'>Zeke The Dev</StyledLink>
-                <StyledLink to='/blog'>Blog</StyledLink>
-                <StyledLink to='/about'>About</StyledLink>
-                <StyledLink to='/contact'>Let's Talk!</StyledLink>
-            </nav>
+            <StyledNav>
+                <MenuLeft>
+                    <MenuLeftLink to='/'>Zeke The Dev</MenuLeftLink>
+                </MenuLeft>
+
+                <MenuRight>                
+                    <MenuRightFirstLink to='/blog'>Blog</MenuRightFirstLink>
+                    <MenuRightMidLink to='/about'>About</MenuRightMidLink>
+                    <MenuRightEndLink to='/contact'>Let's Talk!</MenuRightEndLink>
+                </MenuRight>
+            </StyledNav>
         </StyledHeader>
     )
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,15 @@
 import React from "react"
 import styled from 'styled-components';
-import Header from '../components/header';
-import Footer from '../components/footer';
+import Layout from '../components/layout'
 
 const Index = () => {
-    return(
-        <div>
-            <Header/>
-            <h1>Welcome!</h1>
+    return (
 
+        <Layout>
+            <h1>Welcome!</h1>
             <p>I am Ezekiel Nwafor (call me Zeke). I'm a developer living in Massachusetts. I like to share the things that I'm learning and doing in web development. </p>
-            <Footer />
-        </div>
+        </Layout>
+        
     )
 }
 
