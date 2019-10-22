17d5787fcf8cdd5231a92c2210ff340b1b930a73
Ezekiel-Nwafor <29420639+Ezekiel-Nwafor@users.noreply.github.com>
2019-10-22 15:38:16 -0400

    styling index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,20 @@ import React from "react"
 import styled from 'styled-components';
 import Layout from '../components/layout'
 
+const IntroSection = styled.section`
+    padding-top: 75px;
+`
+
 const Index = () => {
     return (
 
         <Layout>
-            <h1>Welcome!</h1>
-            <p>I am Ezekiel Nwafor (call me Zeke). I'm a developer living in Massachusetts. I like to share the things that I'm learning and doing in web development. </p>
+            <IntroSection>
+                <div>
+                    <h1>Welcome!</h1>
+                    <p>I am Ezekiel Nwafor (call me Zeke). I'm a developer living in Massachusetts. I like to share the things that I'm learning and doing in web development. </p>
+                </div>
+            </IntroSection>
         </Layout>
         
     )
