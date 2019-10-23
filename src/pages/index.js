72c78dda9f11cad598a1c0b610f00e3284d36aa7
Ezekiel-Nwafor <29420639+Ezekiel-Nwafor@users.noreply.github.com>
2019-10-23 07:36:29 -0400

    styling index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,18 @@ import styled from 'styled-components';
 import Layout from '../components/layout'
 
 const IntroSection = styled.section`
-    padding-top: 75px;
+    display: flex;
+    align-items: center;
+    margin-top: 75px;
+`
+
+const StyledDiv = styled.div`
+    max-width: 33%;
+    margin: 0 auto;
+`
+
+const StyledH1 = styled.h1`
+    margin-bottom: 5px;
 `
 
 const Index = () => {
@@ -11,10 +22,10 @@ const Index = () => {
 
         <Layout>
             <IntroSection>
-                <div>
+                <StyledDiv>
                     <h1>Welcome!</h1>
                     <p>I am Ezekiel Nwafor (call me Zeke). I'm a developer living in Massachusetts. I like to share the things that I'm learning and doing in web development. </p>
-                </div>
+                </StyledDiv>
             </IntroSection>
         </Layout>
         
