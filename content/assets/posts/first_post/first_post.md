26feb66dfa64637d165772739c2a6ed040d27f3b
Ezekiel-Nwafor <29420639+Ezekiel-Nwafor@users.noreply.github.com>
2019-11-01 09:26:10 -0400

    added filesystem plugin added markdown transformer plugin

---
+++ content/assets/posts/first_post/first_post.md
@@ -0,0 +1,11 @@
+---
+title: "My First Post"
+date: "10-31-2019"
+---
+
+Hello everyone! This is my first blog post!
+
+##Intro
+1. me
+2. myself
+3. and I
--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,14 @@ module.exports = {
   },
   
   plugins: [
-    'gatsby-plugin-styled-components'
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'content',
+        path: `${__dirname}/content/`
+      }
+    },
+    'gatsby-transformer-remark'
   ],
 }
